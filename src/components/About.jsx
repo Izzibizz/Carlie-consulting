2c3a03a7227aad4d58fd392d81e0e9ff277110eb
izzibizz <contact@izabellind.com>
2025-02-19 17:09:38 +0100

    fixing height for mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@
 export const About = () => {
   return (
     <section className="laptop:h-[100vh] ">
-      <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1739972640/morph-bg3_bw8vba.svg" alt="background for section about" className="absolute z-0  object-cover top-[95vh] left-0 h-[100vh] max-h-[100vh] animate-zoomInOut" />
+      <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1739972640/morph-bg3_bw8vba.svg" alt="background for section about" className="absolute z-0  object-cover top-[80vh] laptop:top-[95vh] left-0 h-[100vh] max-h-[100vh] animate-zoomInOut" />
     <div className="w-11/12  mx-auto grid laptop:grid-cols-2 relative z-20 gap-6 py-32">
         <div className="flex flex-col gap-4">
         <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1739978658/091c9f2741908ab28d38d3b3013aed83_vhdp2b.jpg" alt="Carolina" className="h-[300px] w-[250px] laptop:h-[600px] laptop:w-[400px] object-cover aspect-[3/4]" />
