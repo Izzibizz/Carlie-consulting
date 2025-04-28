edc66125945c7e554a3e5582d70377e221a76afa
izzibizz <contact@izabellind.com>
2025-04-28 12:06:16 +0200

    padding contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import mail from "/Mail.svg"
 
 export const Contact = () => {
   return (
-    <section className="h-[100vh] bg-[url('https://res.cloudinary.com/dwjbiceg4/image/upload/v1745326427/d11601cfa9fc6820ba2ea9f6aea22dbc_aidsru.jpg')] bg-cover">
+    <section className="min-h-[100vh] bg-[url('https://res.cloudinary.com/dwjbiceg4/image/upload/v1745326427/d11601cfa9fc6820ba2ea9f6aea22dbc_aidsru.jpg')] bg-cover pb-40 laptop:pb-0">
       <div className="flex flex-col tablet:flex-row gap-4 w-10/12 mx-auto laptop:w-8/12 py-20 laptop:py-40 justify-between">
       <img src="https://res.cloudinary.com/dwjbiceg4/image/upload/v1745750264/eee_oqulxc.jpg" alt="carlie consulting Emelie & Carolina" className="w-full tablet:w-[400px] aspect-[3/4] object-cover border-4"/>
       <div className="flex flex-col gap-12 items-end justify-end">
